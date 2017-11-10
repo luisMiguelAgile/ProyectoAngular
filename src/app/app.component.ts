8768f6ed6ffd096401f012a50defb7f1367641d6
Luis Miguel Gonzalez <luismiguel.agile@gmail.com>
2017-11-10 17:46:52 -0500

    creacion de directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzisquare';
-  lugares:any =  [
-  	{active: true, nombre: 'Floresteria G'},
-  	{active: true,nombre: 'Donas la pas'},
-  	{active: false, nombre: 'Veterinaria hF'}
+  lugares: any = [
+    { plan: 'gratuito', cercania: 1, distancia: 1, active: true, nombre: 'Floresteria G' },
+    { plan: 'pagado', cercania: 2, distancia: 2, active: true, nombre: 'Donas la pas' },
+    { plan: 'gratuito', cercania: 3, distancia: 3, active: false, nombre: 'Veterinaria hF' }
   ];
 
-  lat:number = 5.4600252;
-  lng:number = -74.342902;
+  lat: number = 5.4600252;
+  lng: number = -74.342902;
 
-  constructor(){
-  	
+  constructor() {
+
   }
 }
